c0eb87ab86709e88f5a735b101f89e9db72297e2
Popular Demand <popdemtech@gmail.com>
2022-08-22 18:02:11 -0600

    Add express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Welcome to SmartSite!');
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', function(request, response) {
+  response.send('<h1>Welcome to SmaRtsite!</h1>');
+});
+
+app.listen(PORT, () => {
+  console.log(`SmaRtsite listening on port ${PORT}`);
+});
